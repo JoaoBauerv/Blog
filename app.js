b6be9e6025e96e6790c58e83104bc355a5bff52f
João Bauer <136642016+JoaoBauerv@users.noreply.github.com>
2025-06-25 20:39:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
 const express = require('express');
 const session = require('express-session');
-const multer = require('multer');
 const bcrypt = require('bcryptjs');
 const path = require('path');
 
 const app = express();
-module.exports = app;
 
 
 
 app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
-app.use(express.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
 
 
-// Multer configuração
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'public/uploads'),
-  filename: (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname))
-});
-const upload = multer({ storage });
 
 // Rotas
 app.get('/', (req, res) => res.render('index'));
 app.get('/posts', (req, res) => res.render('posts'));
 
 
-app.listen(3006, () => console.log('Servidor rodando em http://localhost:3006'));
+//app.listen(3006, () => console.log('Servidor rodando em http://localhost:3006'));
 
 module.exports = app;
