fe8b161544d4a4c8984a1f5b457deb1bcb48d266
joaobauerv <joaovitorverzeletti@gmail.com>
2025-06-25 21:45:23 -0300

    sa

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas
 app.get('/', (req, res) => res.render('index'));
