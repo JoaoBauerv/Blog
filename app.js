ca1fe29acc06ba0b1336564cb7f101b25ec339a6
joao-verzeletti <joaovitorverzeletti@gmail.com>
2025-06-26 13:12:34 -0300

    comments

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rotas
+
 app.get('/', (_, res) => res.render('index'));
 
 
